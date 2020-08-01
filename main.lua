1e2f8cba362e58c159510f6e409b7d1993e94438
bawdeveloppement <baw.developpement@outlook.com>
2020-08-02 01:41:40 +0200

    Appartement

--- main.lua
+++ main.lua
@@ -3,25 +3,76 @@ player = {}
 player.x = 0;
 player.y = 0;
 
+
+
 function love.load()
+    love.window.setMode(400, 400);
+    dimx, dimy = love.window.getMode();
+    cx, cy = dimx / 2, dimy / 2;
+    sx, sy = 0, dimy;
+
+    -- Init Appartements
+    apparts = {}
+    for i=0,5 do
+        appart = {}
+        appart.width = love.math.random(3, 6)
+        appart.height= love.math.random(4, 15)
+        if i ~= 0 then
+            appart.x = apparts[i].x + apparts[i].width * 10 + love.math.random(1,2) * 10 
+        else
+            appart.x = 0
+        end
+        appart.y = dimy + 10
+        table.insert(apparts, appart)
+    end
 end
 
 function love.update(dt)
-    if love.keyboard.isDown("z") then
-        player.y = player.y - 50 * dt; -- DT = Delta Time
-    end
-    if love.keyboard.isDown("s") then
-        player.y = player.y + 50 * dt;
-    end
-    if love.keyboard.isDown("q") then
-        player.x = player.x - 50 * dt;
-    end
-    if love.keyboard.isDown("d") then
-        player.x = player.x + 50 * dt;
-    end
 end
 
+max_appart = 1
+current_appart = 0
+lastx = 0
+lastwidth = love.math.random(3, 5)
+lastheight= love.math.random(4, 15)
+lastwidth2 = love.math.random(3, 5)
+lastheight2= love.math.random(4, 15)
+
+appartdrawn = false
 function love.draw()
-    love.graphics.setColor(1,1,1,1);
-    love.graphics.rectangle("fill", player.x, player.y, 30, 30);
+    
+    -- Appartement
+    for i, v in ipairs (apparts) do
+        draw_appart(v.x, v.y, v.width, v.height)
+    end
+    -- Fin Appartement
+end
+
+function draw_ville ()
+end
+
+function draw_appart (x, y, width, height)
+    love.graphics.setColor(1, 1, 1, 0.5);
+    local i = 0
+    local j = 0
+    while i < height do
+        j = 0
+        while j < width do
+            love.graphics.line(x + j * 10, y - i * 10, x + j * 10, y -  (i * 10 + 10))
+            love.graphics.line(x + j * 10, y - i * 10, x + j * 10 + 10, y - i * 10)
+            if i ~= height - 1 then
+                love.graphics.line(x + j * 10 + 10, y - (i * 10 + 10), x + j * 10 + 10, y - i * 10)
+            end
+            if j ~= width - 1 then
+                love.graphics.line(x + j * 10 + 10, y - (i * 10 + 10), x + j * 10, y - (i * 10 + 10))
+            end
+            if j == width - 1 and i == height - 1 then
+                love.graphics.line(x + j * 10 + 10, y - (i * 10 + 10), x + j * 10, y - (i * 10 + 10))
+                love.graphics.line(x + j * 10 + 10, y - (i * 10 + 10), x + j * 10 + 10, y - i * 10)
+            end
+            j = j + 1
+        end
+        i = i + 1
+    end
+    return 1
 end
