734b91776893431ad00871b17e3db655385abaf3
bawdeveloppement <baw.developpement@outlook.com>
2020-08-03 16:54:53 +0200

    background city : main.vincent.lua

--- main.vincent.lua
+++ main.vincent.lua
@@ -9,6 +9,8 @@ local City      = require('city')
 
 -- Global Value --
 local dimx, dimy = 0, 0;
+local MainCity;
+local BgCity;
 -- End global
 
 local player = love.graphics.newImage("adventurer.png")
@@ -22,14 +24,18 @@ function love.load()
     love.graphics.setBackgroundColor(137/255,175/255,192/255);
     love.window.setMode(800, 600);
     dimx, dimy = love.window.getMode();
-    City = City:new(dimx, dimy);
+    MainCity = City:new(dimx, dimy, {1,1,1,1});
+    BgCity = City:new(dimx, dimy, {0.8,0.8,0.8,1}, 40);
+    BgCity.debug = true -- For text debugging => City content
 end
 
 function love.update(dt)
     Camera:update(dt)
-    City:update(dt)
+    MainCity:update(dt)
+    BgCity:update(dt)
 end
 
 function love.draw()
-    City:draw()
+    BgCity:draw()
+    MainCity:draw()
 end
