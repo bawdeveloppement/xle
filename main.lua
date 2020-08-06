d5a0a04752dd11b65a4f4d8b47b5579f573d399f
bawdeveloppement <baw.developpement@outlook.com>
2020-08-06 18:31:49 +0200

    Update

--- main.lua
+++ main.lua
@@ -1,17 +1,16 @@
-_G.basedir = (...):match("(.-)[^%.]+$")
--- _G.readdir = love.filesystem.getRealDirectory("main.lua")
-
 -- Config --
+-- _G.readdir = love.filesystem.getRealDirectory("main.lua")
 local Json      = require("utils.json")
--- local Config    = Json.parse("./config.json");
+_G.basedir = (...):match("(.-)[^%.]+$")
 love.graphics.setDefaultFilter("nearest")
 -- End Config --
 
 -- Require --
-local Camera    = require('utils.baw.object.primitives.camera').Default
-local City      = require('scenes.main.city')
-local Player    = require('scenes.main.player')
-local SceneManager = require('scenes.manager');
+-- local Camera        = require('utils.baw.object.primitives.camera').Default
+-- local City          = require('scenes.main.city')
+-- local Player        = require('scenes.main.player')
+local MainScene     = require('scenes.main.mainscene'):new("Hello")
+local SceneManager  = require('scenes.manager'):new(MainScene);
 -- End Require --
 
 -- Global Value --
@@ -33,21 +32,23 @@ function love.load()
     -- End settings
     
     -- Settings the game
-    MainCity = City:new(dimx, dimy, {1,1,1,1});
-    BgCity = City:new(dimx, dimy, {0.8,0.8,0.8,1}, 40);
-    Jessica = Player:new(0, 0, MainCity.world);
+    -- MainCity = City:new(dimx, dimy, {1,1,1,1});
+    -- BgCity = City:new(dimx, dimy, {0.8,0.8,0.8,1}, 40);
+    -- Jessica = Player:new(0, 0, MainCity.world);
     -- End settings
 end
 
 function love.update(dt)
-    Camera:update(dt)
-    MainCity:update(dt)
-    BgCity:update(dt)
-    Jessica:update(dt)
+    -- Camera:update(dt)
+    -- MainCity:update(dt)
+    -- BgCity:update(dt)
+    -- Jessica:update(dt)
+    SceneManager:update(dt)
 end
 
 function love.draw()
-    BgCity:draw()
-    MainCity:draw()
-    Jessica:draw()
+    -- BgCity:draw()
+    -- MainCity:draw()
+    -- Jessica:draw()
+    SceneManager:draw()
 end
