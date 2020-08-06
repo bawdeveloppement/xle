f9afa48b442e6b4f361c58c7a39cbde677a2b81f
bawdeveloppement <baw.developpement@outlook.com>
2020-08-06 18:29:09 +0200

    Scene Manager

--- scenes/manager.lua
+++ scenes/manager.lua
@@ -1,19 +1,46 @@
 local SceneManager  = require(_G.basedir .. "utils.middleclass")("SceneManager")
 local Scene = require(_G.basedir .. "scenes.scene");
 
+SceneManager.static.scenes = {}
+
 function SceneManager:initialize(scene)
-    self.scenes = {
-        default = scene or Scene:new()
-    }
-    self.current = "default"
+    if scene ~= nil then
+        table.insert(SceneManager.scenes, scene)
+    end
+    self.current = 1
 end
 
 function SceneManager:update(dt)
-    self.scenes[self.current]:update(dt);
+    if table.getn(SceneManager.scenes) >= 1 then
+        SceneManager.scenes[self.current]:update(dt);
+    end
+end
+
+function SceneManager:register(scene)
+    table.insert(SceneManager.scenes, scene);
 end
 
 function SceneManager:draw()
-    self.scenes[self.current]:draw();
+    local count = 1
+    if table.getn(SceneManager.scenes) >= 1 then
+        SceneManager.scenes[self.current]:draw();
+    end
+    -- for k, v in pairs(SceneManager.scenes) do
+    --     love.graphics.print(v.name, 0, count * 15);
+    --     count = count + 1
+    -- end
+end
+
+function SceneManager:changeScene(scenename)
+    local count = 1
+    if table.getn(SceneManager.scenes) > 2 then
+        for k, v in pairs(SceneManager.scenes) do
+            if v.name == scenename then
+                self.current = count
+            end
+            count = count + 1
+        end
+    end
 end
 
 return SceneManager
