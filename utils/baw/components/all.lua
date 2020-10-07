ee5e3282ded8f2ee2359a0848d75e7dc84798077
bawdeveloppement <baw.developpement@gmail.com>
2020-10-07 17:01:54 +0200

    references of all components

---
+++ utils/baw/components/all.lua
@@ -0,0 +1,7 @@
+return {
+    Transform = require(_G.componentDir .. "transform"),
+    Sprite = require(_G.componentDir .. "sprite"),
+    Text = require(_G.componentDir .. "text"),
+    RigidBody = require(_G.componentDir .. "rigidbody"),
+    Component = require(_G.componentDir .. "component"),
+}
