ec1d813a7ae0ce0a79ccb97964e66327c59ee562
bawdeveloppement <baw.developpement@gmail.com>
2020-10-16 17:08:15 +0200

    refactoring

--- main.lua
+++ main.lua
@@ -4,43 +4,16 @@ _G.engineDir      = _G.baseDir .. "engine."
 _G.assetDir      = _G.baseDir .. "assets."
 _G.componentDir   = _G.engineDir .. "baw.components.";
 
-local Json = require("engine.json");
+local BawEditor = require(_G.engineDir .. "baw.BawEditor");
 
--- Link to lua callbacks
--- https://love2d.org/wiki/love
-local callbacks = {
-    load = {
-        args = nil,
-        actions = {
-            {
-                id="print",
-                nsp="global",
-                arg="dazdza"
-            }
-        }
-    },
-    update = {
-        args = nil,
-        actions = {
-            {
-                id="rectangle",
-                nsp="graphics",
-                args={"fill", 0, 0, 50, 50}
-            }
-        }
-    }
-}
+function love.load(arg)
+    BawEditor = BawEditor:new();
+end
 
-for callback_key, callback_value in pairs(callbacks) do
-    love[callback_key] = function ()
-        for action_key, action_value in ipairs(callback_value.actions) do
-            if action_value.nsp == "global" then
-                _G[action_value.id](action_value.arg)
-            else
-                -- ERROR HERE
-                -- https://coldfix.de/2017/03/02/lua-argument-packing/
-                love[action_value.nsp][action_value.id](action_value.args)
-            end
-        end
-    end
+function love.update(dt)
+    BawEditor:update(dt);
+end
+
+function love.draw()
+    BawEditor:draw();
 end
---
+++ sample_features/bindind_callback/bind_cback.lua
@@ -0,0 +1,46 @@
+-- GLOBAL --
+_G.baseDir      = (...):match("(.-)[^%.]+$")
+_G.engineDir      = _G.baseDir .. "engine."
+_G.assetDir      = _G.baseDir .. "assets."
+_G.componentDir   = _G.engineDir .. "baw.components.";
+
+local Json = require("engine.json");
+
+-- Link to lua callbacks
+-- https://love2d.org/wiki/love
+local callbacks = {
+    load = {
+        args = nil,
+        actions = {
+            {
+                id="print",
+                nsp="global",
+                arg="dazdza"
+            }
+        }
+    },
+    update = {
+        args = nil,
+        actions = {
+            {
+                id="rectangle",
+                nsp="graphics",
+                args={"fill", 0, 0, 50, 50}
+            }
+        }
+    }
+}
+
+for callback_key, callback_value in pairs(callbacks) do
+    love[callback_key] = function ()
+        for action_key, action_value in ipairs(callback_value.actions) do
+            if action_value.nsp == "global" then
+                _G[action_value.id](action_value.arg)
+            else
+                -- ERROR HERE
+                -- https://coldfix.de/2017/03/02/lua-argument-packing/
+                love[action_value.nsp][action_value.id](action_value.args)
+            end
+        end
+    end
+end
